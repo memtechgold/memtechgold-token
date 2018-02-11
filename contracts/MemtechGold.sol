75168fca6db6a088a24d1c9f3ba94855b062894b
Eric Terpstra <ericterpstra@Erics-MacBook-Pro.local>
2018-02-11 14:25:12 -0600

    Added package.json and set MemtechGold vars

---
+++ contracts/MemtechGold.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.17;
+
+import 'zeppelin-solidity/contracts/token/MintableToken.sol';
+
+contract MemtechGold is MintableToken {
+  string public name = "memtech gold";
+  string public symbol = "MTG";
+  uint8 public decimals = 0;
+  uint public INITIAL_SUPPLY = 10000;
+
+  function MemtechGold() public {
+      totalSupply = INITIAL_SUPPLY;
+      balances[msg.sender] = INITIAL_SUPPLY;
+  }
+}
